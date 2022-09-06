c174fe178bc5d70722c5bff754422589f3085347
lbezerra <lbezerra@criptowow>
2022-09-06 22:06:40 +0000

    Comitar ultimos ajustes

---
+++ BlockChain.Api/Controllers/HistoricoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockChain.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoricoController : ControllerBase
+    {
+    }
+}
